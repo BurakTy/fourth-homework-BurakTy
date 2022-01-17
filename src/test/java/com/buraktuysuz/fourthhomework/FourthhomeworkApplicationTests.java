0c8b5e469bba3b0023464a9cac4b8e50cd5ab3e9
BurakTy <tuysuzburak@gmail.com>
2022-01-17 18:26:07 +0300

    Initial commit

---
+++ src/test/java/com/buraktuysuz/fourthhomework/FourthhomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buraktuysuz.fourthhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourthhomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
